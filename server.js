07aca3782ac97b80c938bebaf1311319fdd1dd38
Nicole Gile <nicolegile94@gmail.com>
2020-11-18 22:00:21 -0600

    added helpers

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -18,6 +16,9 @@ const sess = {
     db: sequelize
   })
 };
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
